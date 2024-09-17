e609a37725e52a1c061c8bbc5204a5e9e2e22d7c
flutistar <brandonagalli@gmail.com>
2024-09-17 14:43:12 -0700

    Fixed error on unit testing

--- document-service/documents-ui/tests/unit/DocumentsTable.spec.ts
+++ document-service/documents-ui/tests/unit/DocumentsTable.spec.ts
@@ -5,54 +5,52 @@ import { mockedI18n } from '~~/tests/test-utils/mockedi18n'
 import { formatToReadableDate } from '~/utils/dateHelper'
 const mockDocumentResults = [
   {
-    "consumerDocumentId": "0100000056",
-    "consumerFilename": "Example2.pdf",
-    "consumerFilingDateTime": "2024-08-23T19:00:00+00:00",
+    "consumerDocumentId": "0100000023",
+    "consumerFilenames": ['Example2.pdf'],
+    "consumerFilingDateTime": "2024-08-03T19:00:00+00:00",
     "consumerIdentifier": "NR123",
-    "createDateTime": "2024-08-13T17:55:41+00:00",
+    "createDateTime": "2024-08-13T00:15:17+00:00",
     "documentClass": "NR",
-    "documentServiceId": "DS0000100059",
+    "documentServiceId": "DS0000100024",
     "documentType": "NR_MISC",
     "documentTypeDescription": "Name requests miscellaneous documents",
-    "consumerFileNames": ["Example1.pdf", "Example2.pdf"],
-    "documentUrls": ["https://mockurl.com", "https://mockurl.com"]
+    "documentUrls": ['https://mock.com'],
   },
   {
-    "consumerDocumentId": "0100000056",
-    "consumerFilename": "Example2.pdf",
-    "consumerFilingDateTime": "2024-08-23T19:00:00+00:00",
+    "consumerDocumentId": "0100000024",
+    "consumerFilenames": ['Example2.pdf'],
+    "consumerFilingDateTime": "2024-08-03T19:00:00+00:00",
     "consumerIdentifier": "NR123",
-    "createDateTime": "2024-08-13T17:55:41+00:00",
+    "createDateTime": "2024-08-13T00:15:17+00:00",
     "documentClass": "NR",
-    "documentServiceId": "DS0000100059",
+    "documentServiceId": "DS0000100024",
     "documentType": "NR_MISC",
     "documentTypeDescription": "Name requests miscellaneous documents",
-    "consumerFileNames": ["Example1.pdf", "Example2.pdf"],
-    "documentUrls": ["https://mockurl.com", "https://mockurl.com"]
+    "documentUrls": ['https://mock.com'],
   },
   {
-    "consumerDocumentId": "0100000056",
-    "consumerFilename": "Example2.pdf",
-    "consumerFilingDateTime": "2024-08-23T19:00:00+00:00",
+    "consumerDocumentId": "0100000025",
+    "consumerFilenames": ['Example2.pdf'],
+    "consumerFilingDateTime": "2024-08-03T19:00:00+00:00",
     "consumerIdentifier": "NR123",
-    "createDateTime": "2024-08-13T17:55:41+00:00",
+    "createDateTime": "2024-08-13T00:15:17+00:00",
     "documentClass": "NR",
-    "documentServiceId": "DS0000100059",
+    "documentServiceId": "DS0000100024",
     "documentType": "NR_MISC",
     "documentTypeDescription": "Name requests miscellaneous documents",
-    "consumerFileNames": ["Example1.pdf", "Example2.pdf"],
-    "documentUrls": ["https://mockurl.com", "https://mockurl.com"]
+    "documentUrls": ['https://mock.com'],
   }
 ]
 
 
 describe('DocumentsTable', () => {
-  it('Displays expected content', () => {
+  it('Displays expected content', async () => {
     const { documentSearchResults } = storeToRefs(useBcrosDocuments())
     documentSearchResults.value = mockDocumentResults
 
     const wrapper = mount(DocumentsTable, { global: { plugins: [mockedI18n] } })
 
+    await wrapper.vm.$nextTick()
     // Verify the table is rendered
     expect(wrapper.find('[data-cy="document-search-results"]').exists()).toBe(true)
 
@@ -65,7 +63,7 @@ describe('DocumentsTable', () => {
     expect(firstRowCells[1].text()).toBe(mockDocumentResults[0].consumerDocumentId)
     expect(firstRowCells[2].text()).toBe(mockDocumentResults[0].consumerIdentifier)
     expect(firstRowCells[4].text()).toBe(mockDocumentResults[0].documentTypeDescription)
-    expect(firstRowCells[5].text()).toContain(formatToReadableDate('2024-08-23T19:00:00+00:00', true))
+    expect(firstRowCells[5].text()).toContain(formatToReadableDate('2024-08-03T19:00:00+00:00', true))
 
     wrapper.unmount()
   })
