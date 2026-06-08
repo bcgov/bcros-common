b1126d622088e6e403ceee693dc01c49b37d854a
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2026-06-08 12:45:51 -0700

    tweak

--- document-service/doc-api/update_db.sh
+++ document-service/doc-api/update_db.sh
@@ -3,6 +3,8 @@ COMMAND=${1:-upgrade}
 REVISION=${2:-}
 echo starting $COMMAND $REVISION
 export DEPLOYMENT_ENV=migration
-export PYTHONPATH="$(pwd)/src:$PYTHONPATH"
+export PYTHONPATH=/code/src:$PYTHONPATH
+export FLASK_APP=wsgi:app
+cd /code
 flask db $COMMAND $REVISION
 echo 'upgrade completed'
